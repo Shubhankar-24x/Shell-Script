e65760e1a567699fb74de24004a7a9999dd2368c
Shubhankar-24x <shubhankar2821@gmail.com>
2024-08-27 02:28:54 +0530

    adding date

--- aws_resource_list.sh
+++ aws_resource_list.sh
@@ -3,7 +3,7 @@
 ###############################################################################
 # Author: Shubhankar Biswas
 # Version: v0.0.1
-
+# Date: 27/08/2024
 ## Script to automate the process of listing all the resources in an AWS account
 #
 # Below are the services that are supported by this script:
