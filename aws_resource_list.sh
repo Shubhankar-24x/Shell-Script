8ceb84c6314e9f722fc6f2f683962ff38968d366
Shubhankar-24x <shubhankar2821@gmail.com>
2024-08-27 02:10:51 +0530

    First version of commit

--- aws_resource_list.sh
+++ aws_resource_list.sh
@@ -4,7 +4,7 @@
 # Author: Shubhankar Biswas
 # Version: v0.0.1
 
-# Script to automate the process of listing all the resources in an AWS account
+## Script to automate the process of listing all the resources in an AWS account
 #
 # Below are the services that are supported by this script:
 # 1. EC2
